d64cdd4095c7b61626dce8956c2eb48c8d7259d6
JDLanata <87950670+JDLanata@users.noreply.github.com>
2021-10-02 19:08:01 -0700

    basic index update, added api web link

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,25 @@
 console.log("hello there")
-var apiKey = a94854f2d3d0f7762987fe81bca50927;
+var apiKey = "a94854f2d3d0f7762987fe81bca50927";
 var city;
+var lat;
+var lon;
 
+
+var requestUrl = "api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}"
+console.log(requestUrl);
+function getApi(requestUrl) {
+    // Insert the API url to get a list of your repos
+    
+  
+    fetch(requestUrl)
+      .then(function (response) {
+          console.log(response)
+        return response.json();
+
+      })
+      .then(function (data) {
+        console.log(data);
+      });
+  }
+  
+getApi();
