62bc7ec030ea87e024a163afe7f84dcd49709b71
JDLanata <87950670+JDLanata@users.noreply.github.com>
2021-10-03 18:09:11 -0700

    set up fetch, and locastorage, more format to index

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,24 +2,96 @@ console.log("hello there")
 var apiKey = "a94854f2d3d0f7762987fe81bca50927";
 var city;
 var lat;
-var lon;
+var long;
+var searchArea = $("#searchArea");
+var searchBtn = $("#searchBtn");
+var lastSearch = $("#lastSearch");
+
+console.log(searchArea.val())
 
 
-var requestUrl = "api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}"
-console.log(requestUrl);
-function getApi(requestUrl) {
-    // Insert the API url to get a list of your repos
+// var fiveDayApi = "https://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid="+apiKey;
+
+
+// var apiUrl = "htts://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+apiKey
+var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=London&appid="+apiKey;
+
+console.log(apiUrl)
+searchBtn.click(function(event) {
+  event.preventDefault();
+  city = searchArea.val();
+  console.log(city);
+  cityFind();
+})
+ 
+function cityFind(apiUrl){
+  // console.log(city);
+  fetch(apiUrl)
+  .then(function(response){
+
+  console.log(response);
+    return response.json();
+   })
+
+
+}
+
+
+
+////Store search History in localstorage data
+lastSearch = JSON.parse(localStorage.getItem("weather")) || [];
+
+
+
+function searchHist(event) {
+    event.preventDefault();
+
+    var weatherCity = {
+        place: newPlace
+    
+    }
+
+    lastSearch.push(weatherCity);
+
+    for (let i = 0; i < lastSearch.length; i++) {
+
+        var newLi = $("<li></li>")
+        newLi.append(lastSearch[i].place);
+        searchArea.append(newLi);
+
+    }
+
+    localStorage.setItem("weather", JSON.stringify(lastSearch));
+    
+
+}
+
+
+
+
+
+
+
+
+// var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=london&appid="+apiKey;
+// console.log(requestUrl);
+
+
+// function getApi(requestUrl) {
+
     
   
-    fetch(requestUrl)
-      .then(function (response) {
-          console.log(response)
-        return response.json();
+//     fetch(requestUrl)
+//       .then(function (response) {
+//           console.log(response)
+//         return response.json();
 
-      })
-      .then(function (data) {
-        console.log(data);
-      });
-  }
+//       })
+//       .then(function (data) {
+//         console.log(data);
+//       });
+//   }
   
-getApi();
+// getApi();
+
+
